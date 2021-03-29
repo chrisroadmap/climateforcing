b59fed655dc195c51ca2046c6ce2e27fdade775b
Chris Smith <chrisroadmap@gmail.com>
2021-03-29 23:19:00 +0100

    get rid of masked array option. Probably useful once

--- src/climateforcing/atmos/humidity.py
+++ src/climateforcing/atmos/humidity.py
@@ -5,118 +5,93 @@ CAUTION: after the refactoring these have not been tested.
 
 from numpy import exp, ma, maximum
 
-# TODO: tests
-# TODO: why separate treatment for masked arrays?
 # TODO: Alduchov and Eskridge (1996) reference
 
 
 def specific_to_relative(
-    prsaturation_vapour_pressuresure,
     specific_humidity,
-    air_temperature,
+    pressure=101325,
+    air_temperature=288.15,
     A=17.625,
     B=-30.11,
     C=610.94,
-    masked=False,
+    rh_percent=False,
 ):  # pylint: disable=invalid-name,too-many-arguments
     """Convert specific humidity to relative humidity.
 
     From Mark G. Lawrence, BAMS Feb 2005, eq. (6)
 
     Inputs:
-        prsaturation_vapour_pressuresure :: float or `np.ndarray`
-            Air prsaturation_vapour_pressuresure (Pa)
         specific_humidity :: float or `np.ndarray`
             Specific humidity (kg/kg)
+        pressure :: float or `np.ndarray`
+            Air prsaturation_vapour_pressuresure (Pa)
         air_temperature :: float or `np.ndarray`
             Air temperature (K)
         A, B, C :: float
             Fitting parameters from Alduchov and Eskridge (1996)
-        masked :: bool
-            True if inputs are masked arrays (default False)
+        rh_percent :: bool
+            True to return relative humidity in %, False if 0-1 scale
 
     Returns:
         relative_humidity: relative humidity on unit scale (100% = 1)
     """
-    if not masked:
-        saturation_vapour_pressure = C * exp(
-            A * (air_temperature - 273.15) / (B + air_temperature)
+    saturation_vapour_pressure = C * exp(
+        A * (air_temperature - 273.15) / (B + air_temperature)
+    )
+    saturation_mixing_ratio = (
+        0.62198
+        * saturation_vapour_pressure
+        / (
+            maximum(pressure, saturation_vapour_pressure)
+            - (1 - 0.62198) * saturation_vapour_pressure
         )
-        saturation_mixing_ratio = (
-            0.62198
-            * saturation_vapour_pressure
-            / (
-                maximum(prsaturation_vapour_pressuresure, saturation_vapour_pressure)
-                - (1 - 0.62198) * saturation_vapour_pressure
-            )
-        )
-        relative_humidity = specific_humidity / saturation_mixing_ratio
-    else:
-        saturation_vapour_pressure = C * ma.exp(
-            A * (air_temperature - 273.15) / (B + air_temperature)
-        )
-        saturation_mixing_ratio = (
-            0.62198
-            * saturation_vapour_pressure
-            / (
-                maximum(prsaturation_vapour_pressuresure, saturation_vapour_pressure)
-                - (1 - 0.62198) * saturation_vapour_pressure
-            )
-        )
-        relative_humidity = specific_humidity / saturation_mixing_ratio
+    )
+    relative_humidity = specific_humidity / saturation_mixing_ratio
+    if rh_percent:
+        relative_humidity = relative_humidity * 100
     return relative_humidity
 
 
 def relative_to_specific(
-    prsaturation_vapour_pressuresure,
     relative_humidity,
-    air_temperature,
+    pressure=101325,
+    air_temperature=288.15,
     A=17.625,
     B=-30.11,
     C=610.94,
-    masked=False,
+    rh_percent=False
 ):  # pylint: disable=invalid-name,too-many-arguments
     """Convert relative humidity to specific humidity.
 
     From Mark G. Lawrence, BAMS Feb 2005, eq. (6)
 
     Inputs:
-        prsaturation_vapour_pressuresure :: float or `np.ndarray`
-            Air prsaturation_vapour_pressuresure (Pa)
         relative_humidity :: float or `np.ndarray`
-            Specific humidity (kg/kg)
+            relative humidity (see rh_percent)
+        pressure :: float or `np.ndarray`
+            Air prsaturation_vapour_pressuresure (Pa)
         air_temperature :: float or `np.ndarray`
             Air temperature (K)
         A, B, C :: float
             Fitting parameters from Alduchov and Eskridge (1996)
-        masked :: bool
-            True if inputs are masked arrays (default False)
+        rh_percent :: bool
+            True if relative humidity is given in %, False if 0-1 scale
 
     Returns:
-        specific_humidity: relative humidity on unit scale (100% = 1)
+        specific_humidity: specific humidity (kg/kg)
     """
-    if not masked:
-        saturation_vapour_pressure = C * exp(
-            A * (air_temperature - 273.15) / (B + air_temperature)
+    if rh_percent:
+        relative_humidity = (relative_humidity / 100)
+    saturation_vapour_pressure = C * exp(
+        A * (air_temperature - 273.15) / (B + air_temperature)
+    )
+    specific_humidity = (
+        0.62198
+        * (relative_humidity * saturation_vapour_pressure)
+        / (
+            maximum(pressure, saturation_vapour_pressure)
+            - (1 - 0.62198) * saturation_vapour_pressure
         )
-        specific_humidity = (
-            0.62198
-            * (relative_humidity * saturation_vapour_pressure)
-            / (
-                maximum(prsaturation_vapour_pressuresure, saturation_vapour_pressure)
-                - (1 - 0.62198) * saturation_vapour_pressure
-            )
-        )
-    else:
-        saturation_vapour_pressure = C * ma.exp(
-            A * (air_temperature - 273.15) / (B + air_temperature)
-        )
-        specific_humidity = (
-            0.62198
-            * (relative_humidity * saturation_vapour_pressure)
-            / (
-                maximum(prsaturation_vapour_pressuresure, saturation_vapour_pressure)
-                - (1 - 0.62198) * saturation_vapour_pressure
-            )
-        )
+    )
     return specific_humidity
